da08531526c24b8c71a188701b9f45217956441d
Alderson <m_alderson59387@tss-hr420-dm32.fc.ca>
2018-03-28 15:52:05 -0400

    GOT video build

--- js/main.js
+++ js/main.js
@@ -1,37 +1,77 @@
 (() => {
-console.log("GOT video");
+console.log("got videos");
 
-const vidPlayer = document.querySelector('video'),
-	  playButton = document.querySelectorAll('button')[0],
-	  pauseButton = document.querySelectorAll('button')[1],
-	  rewindButton = document.querySelectorAll('button')[2];
+//add to string prototype to cap first letter
+String.prototype.capIt = function() {return this.replace(this.charAt(), this.charAt().toUpperCase());};
+
+const sigils = document.querySelectorAll('.sigilContainer'),
+	  lightbox = document.querySelector('.lightbox'),
+	  closeLightbox = lightbox.querySelector('.close-lightbox'),
+	  vidPlayer = document.querySelector('video'),
+	  taglines = document.querySelectorAll('.tag'),
+	  playPause = document.querySelector('.playPause')
+	  stopButton = document.querySelector('.stop')
+	  rewindButton = document.querySelector('.rewind')
+	  forwardButton = document.querySelector('.forward');
 
 
-function volOn(){
-	vidPlayer.muted=false; //object.property
-}
+//methods/ functions
+function loadMovie(){
+	//turn on light box
+	lightbox.classList.add('show-lightbox');
 
-function volOff(){
-	vidPlayer.muted=true;
-}
+//grab the right video based on class name- split yields name
+	var house = this.className.split(' ')[1].capIt();
+	taglines.forEach(tagline => tagline.innerHTML = house);
 
-function rewindVid(){
-	vidPlayer.currentTime=0;
-	//vidPlayer.currentTime -= 5; <-- bumps back 5 seconds from wherever you are
-}
+//put path together and make video load and play
+	vidPlayer.src = `videos/House-${house}.${vidPlayer.currentSrc.split('.')[1]}`;
+	vidPlayer.load();
 
-function playVid(){
 	vidPlayer.play();
-}
+} 
 
-function pauseVid(){
+function closeLBox(){
+	lightbox.classList.remove('show-lightbox');
 	vidPlayer.pause();
+	vidPlayer.currentTime=0;
 }
 
-vidPlayer.addEventListener('mouseover', volOn);
-vidPlayer.addEventListener('mouseout', volOff);
+function togglePlay (){
+	var theSVG = this.firstElementChild;
+	if(vidPlayer.paused){
+		vidPlayer.play();
+		theSVG.dataset.icon = "pause-circle";
+	}else{
+		vidPlayer.pause();
+		theSVG.dataset.icon = "play-circle";
+	}
+}
+
+function stopVid(){
+	vidPlayer.pause();
+	vidPlayer.currentTime= 0;
+	var play = playPause.firstElementChild;
+	play.dataset.icon = "play-circle";
+}
+
+function rewindVid() {
+	vidPlayer.currentTime=0;
+
+}
+
+function fastFwd() {
+	
+}
+
+
+//events
+sigils.forEach(sigil => sigil.addEventListener('click', loadMovie));
+closeLightbox.addEventListener('click', closeLBox);
+vidPlayer.addEventListener('ended', closeLBox);
+playPause.addEventListener('click', togglePlay);
+stopButton.addEventListener('click', stopVid);
 rewindButton.addEventListener('click', rewindVid);
-playButton.addEventListener('click', playVid);
-pauseButton.addEventListener('click', pauseVid);
+forwardButton.addEventListener('click', fastFwd);
 
 })();
